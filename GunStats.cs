147de78d8366dcdd114cce25a52aa6ffea17cba5
VisEntities <75583568+VisEntities@users.noreply.github.com>
2024-04-11 19:19:23 +0200

    Update GunStats.cs

--- GunStats.cs
+++ GunStats.cs
@@ -10,7 +10,7 @@ using System.Collections.Generic;
 namespace Oxide.Plugins
 {
     [Info("Gun Stats", "VisEntities", "2.0.0")]
-    [Description("Keeps track of your performance with weapons.")]
+    [Description("Tracks your weapon use history, including kills and hits, and displays it on the weapon itself.")]
 
     public class GunStats : RustPlugin
     {
